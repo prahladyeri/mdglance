7308c19cc1a41ea2a3759e5dfe476fe15d0cd70d
Prahlad Yeri <prahladyeri@yahoo.com>
2026-05-31 13:50:48 +0530

    chore: Added markdig library reference

--- mdglance/MainForm.Designer.cs
+++ mdglance/MainForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(906, 370);
+            this.ClientSize = new System.Drawing.Size(610, 286);
             this.Name = "MainForm";
             this.Text = "MDGlance";
             this.ResumeLayout(false);
