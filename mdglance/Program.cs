9694beceeda91b7df3cb8210b90ab6f54ebe1d19
Prahlad Yeri <prahladyeri@yahoo.com>
2026-06-03 09:30:44 +0530

    chore: added multi-instance check, enhanced error handling and tls support

--- mdglance/Program.cs
+++ mdglance/Program.cs
@@ -6,15 +6,40 @@
  * @date 2026-05-31
  */
 using System;
+using System.Net;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace mdglance
 {
     internal static class Program
     {
+        private static Mutex mutex = null;
+
         [STAThread]
         static void Main()
         {
+            const string mutexName = "Global\\mdglanceMutex";
+            bool createdNew;
+            mutex = new Mutex(true, mutexName, out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("Another instance of the application is already running.", "Instance Already Running",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            Application.ThreadException += (sender, args) =>
+            {
+                MessageBox.Show(args.Exception.ToString(), "Unhandled UI Exception");
+            };
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                Exception ex = args.ExceptionObject as Exception;
+                MessageBox.Show(ex?.ToString() ?? "Unknown error", "Fatal Exception");
+            };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
